c890ee917596c4f1c2f2d6f35c794c9a082af91b
whoseinm <huseynmikayilzade07@gmail.com>
2022-09-07 18:28:21 +0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function select_actor(){
+   var actor = document.getElementById("actors").value
+   console.log(actor)
+   var actor_img =  document.getElementById('img') 
+
+   img.src = "img/" + actor + ".jpg"
+}
